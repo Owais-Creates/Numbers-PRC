43e227d79b546613f6d60661c8e0f9a5beeb9176
Owais <zaidalit00@gmail.com>
2024-04-04 21:59:44 +0530

    ques 2

--- numbers.js
+++ numbers.js
@@ -19,4 +19,22 @@ fibonacci(9) // OUTPUT - 0 1 1 2 3 5 8 13 21
  
 // ---------------------------------------------------------------------------------------------------
 
+// Q2 -  Write a function largestNumber which takes aan array of numbers and returns the  largest number in that array. 
 
+
+const largestNumber = (arr) => {
+    let number = arr[0]
+
+    for (i = 1; i < arr.length; i++) {
+
+        if (arr[i] > number) {
+            number = arr[i]
+        }
+    }
+
+    return `largest number --> ${number}`
+}
+
+console.log(
+    largestNumber([2, 3, 4, 56, 7, 100, 5]) //OUTPUT --> largest number --> 100
+);
